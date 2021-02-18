3a1819bb89210c94307efb55ad0995369913bc75
seshadwi <seshaadwi@gmail.com>
2021-02-18 09:43:07 +0700

    02_praktikum_web_lanjut

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/',function () {
+    echo "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    echo nl2br("Nama: Sesha Dwi Lestari \n \n NIM: 1941720173 \n \n Kelas: TI-2A");
+});
+
+Route::get('/article/{id}', function ($id) {
+    echo ("Ini merupakan halaman artikel dengan id ");
+});
